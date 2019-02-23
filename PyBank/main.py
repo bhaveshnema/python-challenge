53b1536f1d48eb7a0246141ef3847b07b71c65a5
bhaveshnema <46545902+bhaveshnema@users.noreply.github.com>
2019-02-22 22:01:44 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,11 +8,11 @@ total_months = []
 total_dollars = []
 monthly_profit_change = []
 
-# Set the locale module
+# Set the locale module for Total Profit to get number in dollars
 locale.setlocale( locale.LC_ALL, 'en_CA.UTF-8' )
 
 # Set the CSV Path
-pybank_csv = os.path.join("../" "Resources", "budget_data.csv") #
+pybank_csv = os.path.join("../" "Resources", "budget_data.csv")
 
 # Open the CSV with comma as a delimiter
 with open(pybank_csv) as csv_file:
@@ -25,18 +25,17 @@ with open(pybank_csv) as csv_file:
         total_months.append(row[0])
         total_dollars.append(int(row[1]))
 
-    # Iterate through the profits in order to get the monthly change in profits
+    # Iterate through the profitsto get the monthly change in profits
     for i in range(len(total_dollars) - 1):
 
     # Take the difference between two months and append to monthly profit change
          monthly_profit_change.append(total_dollars[i + 1] - total_dollars[i])
 
-# Obtain the max and min of the the monthly profit change list
+# Calculate the max and min of the the monthly profit change list
 max_increase_value = max(monthly_profit_change)
 max_decrease_value = min(monthly_profit_change)
 
 # Correlate max and min to the proper month using month list and index from max and min
-#We use the plus 1 at the end since month associated with change is the + 1 month or next month
 max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
 max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
 
@@ -55,7 +54,7 @@ print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str
 # Output files
 with open('financial_analysis.txt', 'w') as text:
 
-    # Write methods to print to Financial_Analysis_Summary
+    # Write methods to print to Financial Analysis
     text.write("Financial Analysis\n" "------------------------")
     text.write("\n")
     text.write(f"Total Months: {len(total_months)}")
